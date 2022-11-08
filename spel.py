fe3ec35009b5c56914b5c50e9351d7549d065fd6
afkleen <vilhelm.melkeraaenafkleen@skola.taby.se>
2022-11-08 08:46:18 +0100

    Game

--- spel.py
+++ spel.py
@@ -1,46 +1,35 @@
 import random
-<<<<<<< HEAD
 
 namn = input("Vad ska din gubbe heta? -->")
+
+KaraktärVal = input("Ska din karaktär vara en [Krigare], [Trollkarl] eller [Ninja]")
+
 
 # Olika Karaktärsklasser ----------
 class Krigare():
     def __init__(self, namn):
         self.namn = namn
-        self.dmg = random.randint(0.54,1) * 100
+        self.dmg = random.randint(0,1) * 100
         self.hp = 100
 
 class Trollkarl():
-    def __init__(self):
-        self.dmg = random.randint(0.33,0.5) * 100
+    def __init__(self, namn):
+        self.dmg = random.randint(0,1) * 100
         self.hp = 100
 
 class Ninja():
-    def __init__(self):
+    def __init__(self, namn):
         def __init__(self):
             self.dmg = 100
 
 #---------------------------------
 
-# DIN KARAKTÄR ----------
-class Karaktär():
-    def __init__(self):
-        self.level = 1
-        self.name = input("Vad heter din karaktär")
-        self.klass = input("Ska din karaktär vara en [Krigare], [Trollkarl] eller [Ninja]")
-
-#------------------------
-
-def main():
-    spelare = Karaktär()
-    if  == "Krigare":
-        Spelare = Krigare()
-
-=======
-print("Hej11")
-
-print("Nu skriver cHARLIE")
-# aaa
-print('bich ass')
-print('hor unge334487')
->>>>>>> 311b997db5f3ce641ca6e7939d4e0dc8c87b902d
+if KaraktärVal == "Krigare":
+    spelare = Krigare(namn)
+    print("Du är en krigare")
+elif KaraktärVal == "Trollkarl":
+    spelare = Trollkarl(namn)
+    print("Du är en Trollkarl")
+elif KaraktärVal == "Ninja":
+    spelare = Ninja(namn)
+    print("Du är en Ninja")
